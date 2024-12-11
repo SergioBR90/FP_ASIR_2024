73400443ccc93c45e88d52dc838ea3a5a8dfd228
SergioBR90 <smbecerrar01@suarezdefigueroa.es>
2024-12-11 19:59:16 +0100

    Añadir la carpeta php

---
+++ php/ejercicio2.php
@@ -0,0 +1,20 @@
+   <?php 
+        echo "<p>¿Qué significa php?</p>";
+        echo "<p>PHP significa Hypertext Preprocessor, aunque originalmente significaba Personal Home Page. </br>
+         Es un lenguaje de programación de código abierto diseñado especialmente para el desarrollo web. 
+         Con PHP, puedes crear sitios web dinámicos e interactivos, 
+         ya que se ejecuta en el servidor para procesar contenido antes de enviarlo al navegador del usuario.</p>";
+    ?>
+   <?php 
+         echo "<p>¿Para qué sirve echo?</p>";
+
+         echo "<p>En PHP, la función echo se utiliza para mostrar texto, variables o contenido en la pantalla (generalmente, en un navegador web).</br>
+          Es una de las formas más comunes de generar salida en PHP.</p>";
+    ?>
+   <?php 
+         echo "<p>¿Qué ocurre cuando ejecutas el archivo en un servidor?</p>";
+         
+         echo "<p>Cuando ejecutas un archivo PHP en un servidor, 
+         el servidor procesa el código PHP, genera una salida (como HTML),
+          y envía esa salida al navegador para que la muestre al usuario.</p>";
+    ?>
---
+++ php/ejercicio3.php
@@ -0,0 +1,30 @@
+  <?php  
+        //Inicio de código PHP 
+        // Comentario de una sola línea
+
+        echo "<p>¿Qué significa php?</p>";
+
+        /* Comentario de varias líneas*/ 
+        echo "<p>PHP significa Hypertext Preprocessor, aunque originalmente significaba Personal Home Page. </br>
+         Es un lenguaje de programación de código abierto diseñado especialmente para el desarrollo web. 
+         Con PHP, puedes crear sitios web dinámicos e interactivos, 
+         ya que se ejecuta en el servidor para procesar contenido antes de enviarlo al navegador del usuario.</p>";
+
+    ?>
+   <?php 
+         /* Comentario sobre la salida echo */ 
+         echo "<p>¿Para qué sirve echo?</p>";
+
+         echo "<p>En PHP, la función echo se utiliza para mostrar texto, variables o contenido en la pantalla (generalmente, en un navegador web).</br>
+          Es una de las formas más comunes de generar salida en PHP.</p>";
+    ?>
+   <?php 
+           // Comentario sobre la ejecucion del servidor
+         echo "<p>¿Qué ocurre cuando ejecutas el archivo en un servidor?</p>";
+         
+         echo "<p>Cuando ejecutas un archivo PHP en un servidor, 
+         el servidor procesa el código PHP, genera una salida (como HTML),
+          y envía esa salida al navegador para que la muestre al usuario.</p>";
+          //Fin de código PHP
+    ?>
+
